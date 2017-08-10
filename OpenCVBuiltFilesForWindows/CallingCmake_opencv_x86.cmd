4f97dcef3bdc5feecc0fa2c9c163ccd17d17d411
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-08-10 12:13:52 +0900

    Use OpenCV 3.2.0 and update build scripts

--- OpenCVBuiltFilesForWindows/CallingCmake_opencv_x86.cmd
+++ OpenCVBuiltFilesForWindows/CallingCmake_opencv_x86.cmd
@@ -1,11 +1,11 @@
 cd ..
 
-rmdir /S /Q opencv_x64
+rmdir /S /Q opencv_x86
 
-mkdir opencv_x64
-cd opencv_x64
+mkdir opencv_x86
+cd opencv_x86
 
-cmake.exe ../opencv -G "Visual Studio 14 2015 Win64" ^
+cmake.exe ../opencv -G "Visual Studio 14 2015" ^
 -DCMAKE_SUPPRESS_REGENERATION:BOOL=TRUE ^
 -DBUILD_TESTS=OFF ^
 -DBUILD_opencv_apps=OFF ^
